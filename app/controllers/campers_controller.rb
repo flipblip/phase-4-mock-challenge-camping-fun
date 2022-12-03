c019896a0bb636bb8f1eccfc65dcbf3cc0ec604a
fliblip <wickynjy@gmail.com>
2022-12-03 14:49:14 +0300

    added camper controller methods

--- app/controllers/campers_controller.rb
+++ app/controllers/campers_controller.rb
@@ -1,4 +1,26 @@
 class CampersController < ApplicationController
 
-    
+    def index
+        render json: Camper.all, status: :ok
+    end
+
+    def show
+        camper = Camper.find_by(id:params[:id])
+        if camper
+            render json: camper, include: :activities, status: :ok
+        else
+            render json: {error: "Camper not found"}, status: :not_found
+        end
+    end
+
+    def create
+        camper = Camper.create(camper_params)
+        render json: camper, status: :created
+    end
+
+    private
+
+    def camper_params
+        params.permit(:name, :age)
+    end
 end
