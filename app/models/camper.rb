bd1db698c61169449e328523445b97f3aaf98204
fliblip <wickynjy@gmail.com>
2022-12-03 14:22:41 +0300

    added validations

--- app/models/camper.rb
+++ app/models/camper.rb
@@ -1,4 +1,15 @@
 class Camper < ApplicationRecord
     has_many :signups
     has_many :activities, through: :signups
+
+    validates :name, presence: true
+
+    validates :age, presence: true
+    validate :age_range
+
+    def age_range
+        if !(age < 18 && age > 8)
+            errors.add(:age, "Age should be between 8 and 18")
+        end
+    end
 end
