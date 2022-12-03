157ee29e2c135bafbc1dcd8de2d4eb1a40346da1
fliblip <wickynjy@gmail.com>
2022-12-03 17:08:11 +0300

    Permit signups params && Done

--- app/controllers/signups_controller.rb
+++ app/controllers/signups_controller.rb
@@ -7,6 +7,10 @@ class SignupsController < ApplicationController
 
     private
 
+    def signup_params
+        params.permit(:activity_id, :camper_id, :time)
+    end
+
     def render_unprocessable_entity_response(exception)
         render json: {errors: [exception.record.errors]}, status: :unprocessable_entity
     end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
 
   resources :campers, only:[:index, :show, :create]
   resources :activities, only:[:index, :destroy]
-  resources :signups
+  resources :signups, only:[:create]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
