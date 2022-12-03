68817e495d9bfe4c3b6f1da28d9774535e8b1716
fliblip <wickynjy@gmail.com>
2022-12-03 14:05:47 +0300

    db: migrate

--- db/migrate/20221203105447_create_signups.rb
+++ db/migrate/20221203105447_create_signups.rb
@@ -1,6 +1,8 @@
 class CreateSignups < ActiveRecord::Migration[6.1]
   def change
     create_table :signups do |t|
+      t.integer :activity_id
+      t.integer :camper_id
 
       t.timestamps
     end
